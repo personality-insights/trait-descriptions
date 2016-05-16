14ebff26e8c0775ecc4dff5f3cde1b4ee95ae7a2
Ary Pablo Batista <batarypa@ar.ibm.com>
2016-05-16 15:14:46 -0300

    v1.0 release

---
+++ test/test.descriptions.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2016 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const assert = require('chai').assert;
+const log = require('winston');
+
+const PersonalityTraitDescriptions = require('../');
+
+describe('descriptions', () => {
+
+  it('test descriptions', () => {
+    const descriptions = new PersonalityTraitDescriptions();
+
+
+    const testCases = [
+      ['Openness', 'Openness to experience. Higher: Intellectually curious, emotionally-aware, sensitive to beauty and willing to try new things.\nLower: Preferring the plain, straightforward, and obvious over the complex, ambiguous, and subtle.'],
+      ['Hedonism', 'Pleasure or sensuous gratification for oneself.'],
+      ['Artistic interests', 'Appreciation for art and beauty, both man-made and in nature.'],
+      ['Self-efficacy', 'Belief in one\'s own competence.']
+    ];
+
+    testCases.forEach(t => assert.equal(descriptions.description(t[0]), t[1], `Get description for '${t[0]}' trait`));
+  });
+
+});
