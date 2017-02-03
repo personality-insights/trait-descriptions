31db93087556437750e7a9be747ccc8c82067235
German Attanasio Ruiz <germanattanasio@gmail.com>
2017-02-02 23:35:33 -0500

    compile to es5 fixes #3

--- test/test.descriptions.js
+++ test/test.descriptions.js
@@ -17,7 +17,7 @@
 'use strict';
 
 const assert = require('chai').assert;
-const PersonalityTraitDescriptions = require('../lib/index');
+const PersonalityTraitDescriptions = require('../src/index');
 
 describe('names', () => {
 
